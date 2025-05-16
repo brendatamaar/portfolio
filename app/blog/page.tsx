34d348ac02944a32feb6f895e2c3d1488af9e985
brendatamaar <brendatama10@gmail.com>
2025-05-16 10:31:30 +0700

    ref: minor fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,7 @@
 import { draftMode } from 'next/headers'
 import { fetchBlogPosts } from '../../contentful/blogPosts'
 import Header from '@/components/section/Header'
+import Footer from '@/components/section/Footer'
 import { PostCard } from '@/components/ui/post-card'
 
 async function Blog() {
@@ -24,6 +25,7 @@ async function Blog() {
             />
           )
         })}
+        <Footer />
       </div>
     </div>
   )
