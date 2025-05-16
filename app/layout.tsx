e12657344b03d9fc3edd82396f5485c19d22cc06
brendatamaar <brendatama10@gmail.com>
2025-05-16 10:36:03 +0700

    fix build

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import { ThemeProvider } from 'next-themes'
 import { RESUME_DATA } from '@/data/resume-data'
 import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -39,7 +39,7 @@ export default function RootLayout({
           defaultTheme="system"
         >
           {children}
-		  <SpeedInsights />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
       <Analytics />
