1dca4bf045bc76ec1d9a3bbeb558380d24d262ac
brendatamaar <brendatama10@gmail.com>
2026-04-14 21:17:50 +0700

    fix: disable theme transition on change

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,6 +14,7 @@ function RootLayout() {
       attribute="class"
       storageKey="theme"
       defaultTheme="system"
+      disableTransitionOnChange
     >
       <LanguageProvider>
         <Outlet />
